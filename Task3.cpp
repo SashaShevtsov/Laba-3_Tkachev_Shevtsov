00851edb935b7664dfa9f997e1f51b586188cdd1
IvanTkachev <ivan.tkachev1997@yandex.ru>
2015-10-05 20:30:15 +0300

    Update Task3.cpp

--- Task3.cpp
+++ Task3.cpp
@@ -41,7 +41,72 @@ polynomial mySumandComp(int* a, int size_a, int* b, int size_b)
 	}
 	return resPolynom;
 }
+void assertSum(int* a, int* b, int size_a, int size_b, int* c)
+{
+	polynomial result = mySumandComp(a, size_a, b, size_b);
+
+	bool right = true;
+	if (size_a > size_b)
+	{
+		int size = size_a;
+		for (int i = 0; i < size; i++)
+		{
+			if (result.sum[i] != c[i])
+				right = false;
+		}
+
+		if (right)
+			cout << "OK" << endl;
+		else
+			cout << "NE OK" << endl;
+	}
+	else
+	{
+		int size = size_a;
+		for (int i = 0; i < size; i++)
+		{
+			if (result.sum[i] != c[i])
+				right = false;
+		}
+
+		if (right)
+			cout << "OK" << endl;
+		else
+			cout << "NE OK" << endl;
+	}
+}
+
+void assertComp(int* a, int* b, int size_a, int size_b, int* d)
+{
+		polynomial result = mySumandComp(a, size_a, b, size_b);
+		bool right = true;
+			int size = size_a;
+			for (int i = 0; i < size_a*size_b; i++)
+			{
+				if (result.comp[i] != d[i])
+					right = false;
+			}
+
+			if (right)
+				cout << "OK" << endl;
+			else
+				cout << "NE OK" << endl;
+}
+
+
 int main()
 {
+	int a[]{ 1,2,3 };
+	int b[]{ 2,3 };
+	int a1[]{ 1,4,6 };
+	int b1[]{ 2,7,12,9 };
+	int c[]{ 4,5,3 };
+	int d[]{ 10,3 };
+	int c1[]{ 4,15,6 };
+	int d1[]{ 40,62,45,9 };
+	assertSum(a, b, 3, 2, a1);
+	assertComp(a, b, 3, 2, b1);
+	assertSum(c, d, 3, 2, c1);
+	assertComp(c, d, 3, 2, d1);
 	return 0;
 }
